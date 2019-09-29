d0e225676f2aab9692276b3f637939905cb280c0
nkrus <nik1rus2moscow3@gmail.com>
2019-09-29 14:34:23 +0300

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Nik Solo on 29/09/2019.
+//  Copyright © 2019 Nik Solo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
